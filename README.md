99db558b7115a97d35d4c36f385a031e88d7b33c
Michael Walter <miguschweiz@bluewin.ch>
2018-06-11 14:37:41 +0200

    -

--- README.md
+++
@@ -1,2 +0,0 @@
-# PiDPA
-Raspberry Pi Digital Preamp for 'Cirrus Audio Card' and 'Beni_Skate Audio Switch'
--- install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-hd=`pwd`
-cat system/state.conf | sed -e "s#PIDPA_DIR#$hd#" >/tmp/temp.txt
-mv /tmp/temp.txt /etc/init/pidpa_state.conf
