0f984215630e661de920e0462d9b168fea874767
Michael Walter <miguschweiz@bluewin.ch>
2018-06-11 18:59:06 +0200

    -

--- bin/eq.sh
+++ bin/eq.sh
@@ -18,28 +18,28 @@ fi
 if [ "$1" == "on" ]; then
         touch .eq
         amixer -q -Dhw:RPiCirrus cset name='EQ1 B1 Volume' 0
-        amixer -q -Dhw:RPiCirrus cset name='EQ1 B2 Volume' 20
-        amixer -q -Dhw:RPiCirrus cset name='EQ1 B3 Volume' 50
-        amixer -q -Dhw:RPiCirrus cset name='EQ1 B4 Volume' 50
-        amixer -q -Dhw:RPiCirrus cset name='EQ1 B5 Volume' 50
+        amixer -q -Dhw:RPiCirrus cset name='EQ1 B2 Volume' 5
+        amixer -q -Dhw:RPiCirrus cset name='EQ1 B3 Volume' 12
+        amixer -q -Dhw:RPiCirrus cset name='EQ1 B4 Volume' 12
+        amixer -q -Dhw:RPiCirrus cset name='EQ1 B5 Volume' 12
 
         amixer -q -Dhw:RPiCirrus cset name='EQ2 B1 Volume' 0
-        amixer -q -Dhw:RPiCirrus cset name='EQ2 B2 Volume' 20
-        amixer -q -Dhw:RPiCirrus cset name='EQ2 B3 Volume' 50
-        amixer -q -Dhw:RPiCirrus cset name='EQ2 B4 Volume' 50
-        amixer -q -Dhw:RPiCirrus cset name='EQ2 B5 Volume' 50
+        amixer -q -Dhw:RPiCirrus cset name='EQ2 B2 Volume' 5
+        amixer -q -Dhw:RPiCirrus cset name='EQ2 B3 Volume' 12
+        amixer -q -Dhw:RPiCirrus cset name='EQ2 B4 Volume' 12
+        amixer -q -Dhw:RPiCirrus cset name='EQ2 B5 Volume' 12
 else
 	rm .eq 2>/dev/null
-        amixer -q -Dhw:RPiCirrus cset name='EQ1 B1 Volume' 50
-        amixer -q -Dhw:RPiCirrus cset name='EQ1 B2 Volume' 50
-        amixer -q -Dhw:RPiCirrus cset name='EQ1 B3 Volume' 50
-        amixer -q -Dhw:RPiCirrus cset name='EQ1 B4 Volume' 50
-        amixer -q -Dhw:RPiCirrus cset name='EQ1 B5 Volume' 50
+        amixer -q -Dhw:RPiCirrus cset name='EQ1 B1 Volume' 12
+        amixer -q -Dhw:RPiCirrus cset name='EQ1 B2 Volume' 12
+        amixer -q -Dhw:RPiCirrus cset name='EQ1 B3 Volume' 12
+        amixer -q -Dhw:RPiCirrus cset name='EQ1 B4 Volume' 12
+        amixer -q -Dhw:RPiCirrus cset name='EQ1 B5 Volume' 12
 
-        amixer -q -Dhw:RPiCirrus cset name='EQ2 B1 Volume' 50
-        amixer -q -Dhw:RPiCirrus cset name='EQ2 B2 Volume' 50
-        amixer -q -Dhw:RPiCirrus cset name='EQ2 B3 Volume' 50
-        amixer -q -Dhw:RPiCirrus cset name='EQ2 B4 Volume' 50
-        amixer -q -Dhw:RPiCirrus cset name='EQ2 B5 Volume' 50
+        amixer -q -Dhw:RPiCirrus cset name='EQ2 B1 Volume' 12
+        amixer -q -Dhw:RPiCirrus cset name='EQ2 B2 Volume' 12
+        amixer -q -Dhw:RPiCirrus cset name='EQ2 B3 Volume' 12
+        amixer -q -Dhw:RPiCirrus cset name='EQ2 B4 Volume' 12
+        amixer -q -Dhw:RPiCirrus cset name='EQ2 B5 Volume' 12
 fi
 
--- install.sh
+++ install.sh
@@ -1,5 +1,38 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 hd=`pwd`
-cat system/state.conf | sed -e "s#PIDPA_DIR#$hd#" >/tmp/temp.txt
-mv /tmp/temp.txt /etc/init/pidpa_state.conf
+
+echo "### check for latest cirrus driver in /boot/config.txt: "
+egrep "rpi-cirrus-wm5102$" /boot/config.txt 
+if [ $? -eq 1 ];then
+	echo "### install latest raspbian release, and add 'dtoverlay=rpi-cirrus-wm5102' to /boot/config.txt"
+	printf "### cirrus driver not present, do you want to continue ? (y/n):"
+	read a
+	[ "$a" != "y" ] && exit 1
+else
+	echo OK
+fi
+
+
+echo "### set file permissions"
+sudo chown -R pi:pi .
+chmod 755 bin/*
+chmod 755 cirrus/*
+
+echo "### Install pidpa-state service"
+cat system/pidpa-state | sed -e "s#PIDPA_DIR#$hd#" >/tmp/temp.txt
+sudo mv /tmp/temp.txt /etc/init.d/pidpa-state
+sudo chmod 755 /etc/init.d/pidpa-state
+sudo update-rc.d pidpa-state defaults 2>/dev/null
+sudo service pidpa-state start
+
+echo "### install apache config"
+printf "do you want to install new apache config? (y/n): "
+read a
+if [ "$a" == "y" ];then
+	sudo cp /etc/apache2/sites-available/default /etc/apache2/sites-available/default.sav
+	cat system/apache.conf | sed -e "s#PIDPA_DIR#$hd#" >/tmp/temp.txt
+	sudo mv /tmp/temp.txt /etc/apache2/sites-available/default	
+	sudo service apache2 restart 2>/dev/null
+fi
+
