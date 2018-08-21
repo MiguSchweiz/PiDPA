11faef59c604219e116b34c6222a11b5d9d3ac15
MiguSchweiz <miguschweiz@bluewin.ch>
2018-08-21 20:37:58 +0200

    -

--- bin/kodiTitle.sh
+++ bin/kodiTitle.sh
@@ -3,6 +3,7 @@ cd "$(dirname "$0")"
 cd ..
 
 while [ true ]; do
-	curl -s --data-binary '{"jsonrpc":"2.0","method":"Player.GetItem","params":{"playerid":0},"id":"1"}' -H 'content-type: application/json;' http://192.168.1.24:8080/jsonrpc|python -c "import sys, json; print json.load(sys.stdin)['result']['item']['label']">www/title.htm
+	tit=`curl -s --data-binary '{"jsonrpc":"2.0","method":"Player.GetItem","params":{"playerid":0},"id":"1"}' -H 'content-type: application/json;' http://192.168.1.24:8080/jsonrpc|python -c "import sys, json; reload(sys); sys.setdefaultencoding('utf8');print json.load(sys.stdin)['result']['item']['label']"|iconv -f utf8 -t iso_8859-15`
+	echo $tit>www/title.htm
 	sleep 5
 done
