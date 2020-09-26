09616b769d66d5d7ceb8eb20d5cd97acc06b60c9
MiguSchweiz <miguschweiz@bluewin.ch>
2020-09-26 12:17:32 +0200

    -

--- bin/roon.sh
+++ bin/roon.sh
@@ -2,4 +2,5 @@
 exec 2>&1 >/dev/null
 cd "$(dirname "$0")"
 cvlc alsa://plughw:1,0 -A alsa --alsa-audio-device dmixer &
-ps -ef |grep -v grep|grep roonTitle.sh >/dev/null || ./roonTitle.sh &
+#ps -ef |grep -v grep|grep roonTitle.sh >/dev/null || ./roonTitle.sh &
+./roonTitle.sh &
