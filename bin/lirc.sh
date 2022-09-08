811c2041687935fea0d181a43a35f51e8303d2b5
MiguSchweiz <miguschweiz@bluewin.ch>
2022-09-08 19:59:28 +0100

    -

--- bin/lirc.sh
+++ bin/lirc.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-lircd -n --listen -H irtoy -d /dev/ttyACM0 /etc/lirc/lircd.conf
+#lircd -n --listen -H irtoy -d /dev/ttyACM0 /etc/lirc/lircd.conf
+lircd -n --listen -H default -d /dev/lirc0 /etc/lirc/lircd.conf
