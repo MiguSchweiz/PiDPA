c31580907dda26d25385cc459b3ae264a9d4ca92
MiguSchweiz <miguschweiz@bluewin.ch>
2022-09-25 13:12:31 +0200

    -

--- bin/lsalsa.sh
+++ bin/lsalsa.sh
@@ -3,7 +3,7 @@ for i in /proc/[0-9]*/fd/*
 do
         if readlink $i | grep -q /dev/snd/pcm
         then
-                IFS=/; set -- $i; unset IFS; echo $3
+                IFS=/; set -- $i; unset IFS; ps -ef|grep $3 |grep -v grep
         fi
 done
 
