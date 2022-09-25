d17fa30d513d9d2259d1630c736041557d81aeec
MiguSchweiz <miguschweiz@bluewin.ch>
2022-09-25 17:04:36 +0200

    -

--- bin/lsalsa.sh
+++ bin/lsalsa.sh
@@ -1,9 +1,2 @@
 #!/bin/sh
-for i in /proc/[0-9]*/fd/*
-do
-        if readlink $i | grep -q /dev/snd/pcm
-        then
-                IFS=/; set -- $i; unset IFS; ps -ef|grep $3 |grep -v grep
-        fi
-done
-
+sudo lsof /dev/snd/pcm*
